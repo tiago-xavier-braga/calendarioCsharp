e87eca2844b000033968f72979ce04f7f063904d
Tiago Braga <braga.tiagoxavier@gmail.com>
2022-03-17 19:18:00 -0300

    optimization

--- Program.cs
+++ Program.cs
@@ -3,48 +3,49 @@ using System;
 public class Program {
     public static void Main (string[] args) {    
 
-        DateTime dateTimeExemple = new DateTime(2020, 8, 1);
+        DateTime dateTimeExemple = new DateTime(2022, 2, 1);
         
 
         int yearDate = dateTimeExemple.Year;
         int monthDate = dateTimeExemple.Month; 
         int dayInMonth = System.DateTime.DaysInMonth(yearDate, monthDate);
-        int dayInMonthNumber = 0;
+        int weekDay = 0;
+
+        
         Console.WriteLine("---------------------------------------------------");
         Console.WriteLine("D\tS\tT\tQ\tQ\tS\tS");
         Console.WriteLine("---------------------------------------------------");
         
         if(dateTimeExemple.ToString("dddd") == "segunda-feira"){
           Console.Write("\t");
-          dayInMonthNumber = 2;
+          weekDay = 2;
         } else if (dateTimeExemple.ToString("dddd") == "terça-feira"){
           Console.Write("\t\t");
-          dayInMonthNumber = 3;
+          weekDay = 3;
         } else if (dateTimeExemple.ToString("dddd") == "quarta-feira"){
           Console.Write("\t\t\t");
-          dayInMonthNumber = 4;
+          weekDay = 4;
         } else if (dateTimeExemple.ToString("dddd") == "quinta-feira"){
           Console.Write("\t\t\t\t");
-          dayInMonthNumber = 5;
+          weekDay = 5;
         } else if (dateTimeExemple.ToString("dddd") == "sexta-feira"){
           Console.Write("\t\t\t\t\t");
-          dayInMonthNumber = 6;
+          weekDay = 6;
         } else if (dateTimeExemple.ToString("dddd") == "sábado"){
           Console.Write("\t\t\t\t\t\t");
-          dayInMonthNumber = 7;
+          weekDay = 7;
         } else {
-          dayInMonthNumber = 1;
+          weekDay = 1;
         }
-
-        int j = dayInMonthNumber; 
+ 
         for(int i = 1; i <= dayInMonth; i++){
           Console.Write(i);
           Console.Write("\t");
 
-          if(j % 7 == 0){
+          if(weekDay % 7 == 0){
             Console.Write("\n");
           }
-          j+=1;
+          weekDay += 1;
         }
         
         Console.Write("\n");
