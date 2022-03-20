1400b19cfa06fac4b8c9c13ed9615cd96efee893
Lucas-Emanuel-360 <emanuellucas72902@gmail.com>
2022-03-20 08:53:48 -0300

    falta so a parte final

--- Calendario.cs
+++ Calendario.cs
@@ -1,30 +1,38 @@
 using System;
 using System.Reflection;
 
-public class Calendario {
-    
+public class Calendario
+{
+
     public Byte Mes { get; set; }
     public UInt16 Ano { get; set; }
 
-    public Boolean isMesValido(){
-        if (this.Mes < 13 && this.Mes > 0){
+    public Boolean isMesValido()
+    {
+        if (this.Mes < 13 && this.Mes > 0)
+        {
             return true;
         }
-        else {
-            return false;
-        }
-    }
-    
-    public Boolean isAnoValido(){ 
-        if (this.Ano < 9999 && this.Ano > 0){
-            return true;
-        }
-        else {
+        else
+        {
             return false;
         }
     }
 
-    public string gerarCalendario(){
+    public Boolean isAnoValido()
+    {
+        if (this.Ano < 9999 && this.Ano > 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public string gerarCalendario()
+    {
         //recebe da classe
         int yearDate = this.Ano;
         int monthDate = this.Mes;
@@ -39,44 +47,60 @@ public class Calendario {
         //datatime que recebe os valores solicitados
         DateTime dateTimeExemple = new DateTime(yearDate, monthDate, 1);
 
-        
-        if(dateTimeExemple.ToString("dddd") == "segunda-feira"){
-          textReturn += "\t" ;
-          weekDay = 2;
-        } else if (dateTimeExemple.ToString("dddd") == "terça-feira"){
-          textReturn += "\t\t";
-          weekDay = 3;
-        } else if (dateTimeExemple.ToString("dddd") == "quarta-feira"){
-          textReturn += "\t\t\t";
-          weekDay = 4;
-        } else if (dateTimeExemple.ToString("dddd") == "quinta-feira"){
-          textReturn += "\t\t\t\t";
-          weekDay = 5;
-        } else if (dateTimeExemple.ToString("dddd") == "sexta-feira"){
-          textReturn += "\t\t\t\t\t";
-          weekDay = 6;
-        } else if (dateTimeExemple.ToString("dddd") == "sábado"){
-          textReturn += "\t\t\t\t\t\t";
-          weekDay = 7;
-        } else {
-          weekDay = 1;
-        }
- 
-        for(int i = 1; i <= dayInMonth; i++){
-          textReturn += i;
-          textReturn += "\t";
 
-          if(weekDay % 7 == 0){
-            textReturn += "\n";
-          }
-          weekDay += 1;
+        if (dateTimeExemple.ToString("dddd") == "segunda-feira")
+        {
+            textReturn += "\t";
+            weekDay = 2;
+        }
+        else if (dateTimeExemple.ToString("dddd") == "terça-feira")
+        {
+            textReturn += "\t\t";
+            weekDay = 3;
+        }
+        else if (dateTimeExemple.ToString("dddd") == "quarta-feira")
+        {
+            textReturn += "\t\t\t";
+            weekDay = 4;
+        }
+        else if (dateTimeExemple.ToString("dddd") == "quinta-feira")
+        {
+            textReturn += "\t\t\t\t";
+            weekDay = 5;
+        }
+        else if (dateTimeExemple.ToString("dddd") == "sexta-feira")
+        {
+            textReturn += "\t\t\t\t\t";
+            weekDay = 6;
+        }
+        else if (dateTimeExemple.ToString("dddd") == "sábado")
+        {
+            textReturn += "\t\t\t\t\t\t";
+            weekDay = 7;
+        }
+        else
+        {
+            weekDay = 1;
+        }
+
+        for (int i = 1; i <= dayInMonth; i++)
+        {
+            textReturn += i;
+            textReturn += "\t";
+
+            if (weekDay % 7 == 0)
+            {
+                textReturn += "\n";
+            }
+            weekDay += 1;
         }
 
         textReturn += "\n";
         return textReturn;
     }
-    
-    public override String ToString(){
-      return gerarCalendario();
+
+    public override String ToString()
+    {
+        return gerarCalendario();
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,20 +1,33 @@
-using System;    
+using System;
 
-public class Program {
-    public static void Main (string[] args) {    
+public class Program
+{
+    public static void Main(string[] args)
+    {
 
-        //Calendario c = new Calendario();
-        //c.Ano = 2005;
-        //c.Mes = 3;
+
+    Inicio:
+        // variavel usada para repetição
+        String rep;
+        Calendario c = new Calendario();
+        Console.WriteLine("Insira o ano desejado: ");
+        c.Ano = Convert.ToUInt16(Console.ReadLine());
+        Console.WriteLine("Insira o mes desejado: ");
+        c.Mes = Convert.ToByte(Console.ReadLine());
         DateTime dateTimeExemple = new DateTime(2005, 3, 1);
         string nameMonth = dateTimeExemple.ToString("MMMM");
         Console.WriteLine("---------------------------------------------------");
         Console.WriteLine("D\tS\tT\tQ\tQ\tS\tS");
         Console.WriteLine("---------------------------------------------------");
-      
+        Console.WriteLine(c.gerarCalendario());
+        // estrutura usada para decidir se o usuario ira repetir
+        Console.WriteLine("Deseja iniciar o programa novamente(y/n): ");
+        rep = Console.ReadLine();
+        if (rep == "y")
+        {
+            goto Inicio;
+        }
+        else { }
 
-      
-
-
-  }
-}  
+    }
+}
